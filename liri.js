cf8fcd27e3198865f62d5b8537d0b356aec68a9a
giantrotta24 <giantrotta@me.com>
2019-01-09 15:50:47 -0500

    different color text + non working dowhatitsays function

--- liri.js
+++ liri.js
@@ -7,6 +7,8 @@ const axios = require('axios');
 const moment = require('moment');
 const inquirer = require('inquirer');
 const fs = require('fs');
+const NodeGeocoder = require('node-geocoder');
+const weather = require('weather-js');
 
 //global vars 
 var track = '';
@@ -50,9 +52,9 @@ if (input[2] === 'start') {
             user = inquirerResponse.username;
             console.log("\r\n\r\n\r\n");
             console.log('------------------------------');
-            console.log("Hi " + user + ", I'm LIRI.");
-            console.log('Type node liri.js more-info for help.')
-            console.log('------------------------------');
+            console.log('\x1b[31m', "Hi " + user + ", I'm LIRI.");
+            console.log('\x1b[34m','Type node liri.js more-info for help.')
+            console.log('\x1b[0m', '------------------------------');
             console.log("\r\n\r\n\r\n");
             fs.appendFile('log.txt', 'user logged: ' + user + '.' + '\n', 'utf8', (err) => {
                 if (err) throw err;
@@ -64,11 +66,11 @@ if (input[2] === 'start') {
 if (input[2] === 'more-info') {
     console.log("\r\n\r\n\r\n");
     console.log('------------------------------');
-    console.log('Here are some of the things I can help you with!')
-    console.log('Type node liri.js concert-this to find when your favorite artists are performing next.')
-    console.log('Type node liri.js movie-this to find more info on your favorite movies.')
-    console.log('Type node liri.js spotify-this-song to look up tracks on spotify.')
-    console.log('------------------------------');
+    console.log('\x1b[31m', 'Here are some of the things I can help you with!')
+    console.log('\x1b[32m', 'Type node liri.js concert-this to find when your favorite artists are performing next.')
+    console.log('\x1b[33m', 'Type node liri.js movie-this to find more info on your favorite movies.')
+    console.log('\x1b[34m', 'Type node liri.js spotify-this-song to look up tracks on spotify.')
+    console.log('\x1b[0m', '------------------------------');
     console.log("\r\n\r\n\r\n");
     fs.appendFile('log.txt', 'More Info Request.' + '\n', 'utf8', (err) => {
         if (err) throw err;
@@ -91,24 +93,24 @@ if (input[2] === 'concert-this') {
             .then(function (response) {
 
                 console.log("\r\n\r\n\r\n");
-                console.log('Here are some upcoming ' + artist + ' events' + '.');
+                console.log('\x1b[31m', 'Here are some upcoming ' + artist + ' events' + '.');
                 console.log("\r\n\r\n\r\n");
-                console.log('------------------------------');
-                console.log(moment(response.data[0].datetime).format('MM/DD/YYYY, h:mm a'));
-                console.log('Venue: ' + response.data[0].venue.name);
-                console.log('City: ' + response.data[0].venue.city);
+                console.log('\x1b[0m', '------------------------------');
+                console.log('\x1b[32m', moment(response.data[0].datetime).format('MM/DD/YYYY, h:mm a'));
+                console.log('\x1b[35m', 'Venue: ' + response.data[0].venue.name);
+                console.log('\x1b[0m', 'City: ' + response.data[0].venue.city);
                 console.log('Region: ' + response.data[0].venue.region);
                 console.log('Country: ' + response.data[0].venue.country);
                 console.log('------------------------------');
-                console.log(moment(response.data[1].datetime).format('MM/DD/YYYY, h:mm a'));
-                console.log('Venue: ' + response.data[1].venue.name);
-                console.log('City: ' + response.data[1].venue.city);
+                console.log('\x1b[32m', moment(response.data[1].datetime).format('MM/DD/YYYY, h:mm a'));
+                console.log('\x1b[35m', 'Venue: ' + response.data[1].venue.name);
+                console.log('\x1b[0m', 'City: ' + response.data[1].venue.city);
                 console.log('Region: ' + response.data[1].venue.region);
                 console.log('Country: ' + response.data[1].venue.country);
                 console.log('------------------------------');
-                console.log(moment(response.data[2].datetime).format('MM/DD/YYYY, h:mm a'));
-                console.log('Venue: ' + response.data[2].venue.name);
-                console.log('City: ' + response.data[2].venue.city);
+                console.log('\x1b[32m', moment(response.data[2].datetime).format('MM/DD/YYYY, h:mm a'));
+                console.log('\x1b[35m', 'Venue: ' + response.data[2].venue.name);
+                console.log('\x1b[0m', 'City: ' + response.data[2].venue.city);
                 console.log('Region: ' + response.data[2].venue.region);
                 console.log('Country: ' + response.data[2].venue.country);
                 console.log('------------------------------');
@@ -120,8 +122,8 @@ if (input[2] === 'concert-this') {
             .catch(function (error) {
                 console.log("\r\n\r\n\r\n");
                 console.log('------------------------------');
-                console.log('Error occured: ' + 'No upcoming events detected');
-                console.log('------------------------------');
+                console.log('\x1b[31m', 'Error occured: ' + 'No upcoming events detected');
+                console.log('\x1b[0m', '------------------------------');
                 console.log("\r\n\r\n\r\n");
                 
             });
@@ -129,9 +131,9 @@ if (input[2] === 'concert-this') {
     else {
         console.log("\r\n\r\n\r\n");
         console.log('------------------------------');
-        console.log('You did not include an artist.');
-        console.log('Please try again.');
-        console.log('------------------------------');
+        console.log('\x1b[31m', 'You did not include an artist.');
+        console.log('\x1b[31m', 'Please try again.');
+        console.log('\x1b[0m', '------------------------------');
         console.log("\r\n\r\n\r\n");
         fs.appendFile('log.txt', 'Concert This Request - failed.' + '\n', 'utf8', (err) => {
             if (err) throw err;
@@ -154,18 +156,18 @@ if (input[2] === 'movie-this') {
         axios.get(queryURL)
             .then(function (response) {
                 console.log("\r\n\r\n\r\n");
-                console.log('Here is what I found for ' + movie + '.');
+                console.log('\x1b[31m', 'Here is what I found for ' + movie + '.');
                 console.log("\r\n\r\n\r\n");
-                console.log('------------------------------');
-                console.log('Title: ' + response.data.Title);
-                console.log('Release Year: ' + response.data.Year);
-                console.log('IMDB Rating: ' + response.data.Ratings[0].Value);
-                console.log('Tomato Meter: ' + response.data.Ratings[1].Value);
-                console.log('Country: ' + response.data.Country);
-                console.log('Language: ' + response.data.Language);
-                console.log('Actors: ' + response.data.Actors);
-                console.log('Plot: ' + response.data.Plot);
-                console.log('------------------------------');
+                console.log('\x1b[0m', '------------------------------');
+                console.log('\x1b[32m', 'Title: ' + response.data.Title);
+                console.log('\x1b[34m', 'Release Year: ' + response.data.Year);
+                console.log('\x1b[33m', 'IMDB Rating: ' + response.data.Ratings[0].Value);
+                console.log('\x1b[31m', 'Tomato Meter: ' + response.data.Ratings[1].Value);
+                console.log('\x1b[35m', 'Country: ' + response.data.Country);
+                console.log('\x1b[36m', 'Language: ' + response.data.Language);
+                console.log('\x1b[37m', 'Actors: ' + response.data.Actors);
+                console.log('\x1b[32m', 'Plot: ' + response.data.Plot);
+                console.log('\x1b[0m', '------------------------------');
                 console.log("\r\n\r\n\r\n");
                 fs.appendFile('log.txt', 'Movie This Request: ' + movie + '.' + '\n', 'utf8', (err) => {
                     if (err) throw err;
@@ -174,8 +176,8 @@ if (input[2] === 'movie-this') {
             .catch(function (error) {
                 console.log("\r\n\r\n\r\n");
                 console.log('------------------------------');
-                console.log('Error occured: ' + error);
-                console.log('------------------------------');
+                console.log('\x1b[31m', 'Error occured: ' + error);
+                console.log('\x1b[0m', '------------------------------');
                 console.log("\r\n\r\n\r\n");
             });
     } //if no movie is provided show Mr. Nobody
@@ -187,26 +189,26 @@ if (input[2] === 'movie-this') {
         axios.get(queryURL)
             .then(function (response) {
                 console.log("\r\n\r\n\r\n");
-                console.log('You did not include a movie.');
-                console.log('Check this movie out, it is available on Netflix!');
+                console.log('\x1b[31m', 'You did not include a movie.');
+                console.log('\x1b[31m', 'Check this movie out, it is available on Netflix!');
                 console.log("\r\n\r\n\r\n");
-                console.log('------------------------------');
-                console.log('Title: ' + response.data.Title);
-                console.log('Release Year: ' + response.data.Year);
-                console.log('IMDB Rating: ' + response.data.Ratings[0].Value);
-                console.log('Metascore: ' + response.data.Ratings[1].Value);
-                console.log('Country: ' + response.data.Country);
-                console.log('Language: ' + response.data.Language);
-                console.log('Actors: ' + response.data.Actors);
-                console.log('Plot: ' + response.data.Plot);
-                console.log('------------------------------');
+                console.log('\x1b[0m', '------------------------------');
+                console.log('\x1b[32m', 'Title: ' + response.data.Title);
+                console.log('\x1b[34m', 'Release Year: ' + response.data.Year);
+                console.log('\x1b[33m', 'IMDB Rating: ' + response.data.Ratings[0].Value);
+                console.log('\x1b[31m', 'Metascore: ' + response.data.Ratings[1].Value);
+                console.log('\x1b[35m', 'Country: ' + response.data.Country);
+                console.log('\x1b[36m', 'Language: ' + response.data.Language);
+                console.log('\x1b[37m', 'Actors: ' + response.data.Actors);
+                console.log('\x1b[32m', 'Plot: ' + response.data.Plot);
+                console.log('\x1b[0m', '------------------------------');
                 console.log("\r\n\r\n\r\n");
             })
             .catch(function (error) {
                 console.log("\r\n\r\n\r\n");
                 console.log('------------------------------');
-                console.log('Error occured: ' + error);
-                console.log('------------------------------');
+                console.log('\x1b[31m', 'Error occured: ' + error);
+                console.log('\x1b[0m', '------------------------------');
                 console.log("\r\n\r\n\r\n");
                 fs.appendFile('log.txt', 'Movie This Request Failed.' + '\n', 'utf8', (err) => {
                     if (err) throw err;
@@ -233,14 +235,14 @@ if (input[2] === 'spotify-this-song') {
         }) //take response and log appropriate info
             .then(function (response) {
                 console.log("\r\n\r\n\r\n");
-                console.log('Here is what I found for ' + track + '.');
+                console.log('\x1b[31m', 'Here is what I found for ' + track + '.');
                 console.log("\r\n\r\n\r\n");
-                console.log('------------------------------');
-                console.log('Track: ' + response.tracks.items[0].name);
-                console.log('Artist: ' + response.tracks.items[0].artists[0].name);
-                console.log('Spotify Link: ' + response.tracks.items[0].external_urls.spotify);
-                console.log('Album: ' + response.tracks.items[0].album.name);
-                console.log('------------------------------');
+                console.log('\x1b[0m', '------------------------------');
+                console.log('\x1b[34m', 'Track: ' + response.tracks.items[0].name);
+                console.log('\x1b[33m', 'Artist: ' + response.tracks.items[0].artists[0].name);
+                console.log('\x1b[32m', 'Spotify Link: ' + response.tracks.items[0].external_urls.spotify);
+                console.log('\x1b[35m', 'Album: ' + response.tracks.items[0].album.name);
+                console.log('\x1b[0m', '------------------------------');
                 console.log("\r\n\r\n\r\n");
                 fs.appendFile('log.txt', 'Spotify This Song Request: ' + track + '.' + '\n', 'utf8', (err) => {
                     if (err) throw err;
@@ -249,8 +251,8 @@ if (input[2] === 'spotify-this-song') {
             .catch(function (err) {
                 console.log("\r\n\r\n\r\n");
                 console.log('------------------------------');
-                console.log('Error occured: ' + err);
-                console.log('------------------------------');
+                console.log('\x1b[31m', 'Error occured: ' + err);
+                console.log('\x1b[0m', '------------------------------');
                 console.log("\r\n\r\n\r\n");
             });
 
@@ -261,26 +263,26 @@ if (input[2] === 'spotify-this-song') {
             query: 'The Sign',
             limit: 10
         }).then(function (response) {
-            onsole.log("\r\n\r\n\r\n");
-            // console.log(response.tracks.items[9]);
-            console.log('You did not include a track.');
-            console.log('Check out this awesome track!');
             console.log("\r\n\r\n\r\n");
-            console.log('------------------------------');
-            console.log('Track: ' + response.tracks.items[9].name);
-            console.log('Artist: ' + response.tracks.items[9].artists[0].name);
-            console.log('Spotify Link: ' + response.tracks.items[9].external_urls.spotify);
-            console.log('Album: ' + response.tracks.items[9].album.name);
-            console.log('------------------------------');
-            onsole.log("\r\n\r\n\r\n");
+            // console.log(response.tracks.items[9]);
+            console.log('\x1b[31m', 'You did not include a track.');
+            console.log('\x1b[31m', 'Check out this awesome track!');
+            console.log("\r\n\r\n\r\n");
+            console.log('\x1b[0m', '------------------------------');
+            console.log('\x1b[34m', 'Track: ' + response.tracks.items[9].name);
+            console.log('\x1b[33m', 'Artist: ' + response.tracks.items[9].artists[0].name);
+            console.log('\x1b[32m', 'Spotify Link: ' + response.tracks.items[9].external_urls.spotify);
+            console.log('\x1b[35m', 'Album: ' + response.tracks.items[9].album.name);
+            console.log('\x1b[0m', '------------------------------');
+            console.log("\r\n\r\n\r\n");
             fs.appendFile('log.txt', 'Spotify This Song Request: Track Request Failed.' + '\n', 'utf8', (err) => {
                 if (err) throw err;
             });
         }).catch(function (err) {
             console.log("\r\n\r\n\r\n");
             console.log('------------------------------');
-            console.log('Error occured: ' + err);
-            console.log('------------------------------');
+            console.log('\x1b[31m', 'Error occured: ' + err);
+            console.log('\x1b[0m', '------------------------------');
             console.log("\r\n\r\n\r\n");
             
         });
